228f40dd420029a44f2d0b1425e025d94af909ed
Ryan Sommers <ryansommers@gmail.com>
2016-11-24 01:36:38 -0500

    Fleshed out mapping methods'

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
-import random
-from config import MIN_LATITUDE, MAX_LATITUDE, MIN_LONGITUDE, MAX_LONGITUDE
+import random, json, urllib.request
+from config import MIN_LATITUDE, MAX_LATITUDE, MIN_LONGITUDE, MAX_LONGITUDE, 
+	GEOCODING_API_PREFIX, GEOCODING_API_KEY
 
 def randomLocation():
 	## Returns tuple of randomly selected lat/long point
@@ -18,7 +19,39 @@ def buildTrip():
 	
 def revGeocode(latLong):
 	## Takes a tuple of latitude, longitude and returns string of place name
-	pass
+	response = getGeoCodeResponse(latLong)
+	return parseGeoCodeResponse(response)
+
+def getGeoCodeResponse(latLong):
+	## Takes a tuple of latitude, longitude, builds request and gets geocode data	
+	request = 
+		{ 
+		'prefix' : GEOCODING_API_PREFIX,
+		'latitude' : latLong[0], 
+		'longitude' : latLong[1], 
+		'key' : GEOCODING_API_KEY
+		}
+	
+	requestURL = '{prefix}latlng={latitude},{longitude}&key={key}'.format(**request)
+	
+	with urllib.request.urlopen(requestURL) as response:
+		geoData = response.read().decode('utf8')
+	
+	parsed = json.loads(geoData)
+	
+	return parsed
+	
+def parseGeoCodeResponse(response)
+	## Parses a response from getGeoCodeResponse
+	if not 'results' in response:
+		return None
+	
+	if len(response['results']) > 1:
+		return response['results'][1]['formatted_address']
+		
+	else:
+		return response['results'][0]['formatted_address']
+	
 
 def generateWaypoints(num):
 	## Generator for num waypoints
