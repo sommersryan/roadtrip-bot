fe630a3ff8f46257f286997b4fd7c1b6187a9b0b
Ryan Sommers <ryansommers@gmail.com>
2016-11-23 23:37:48 -0500

    Added basic config

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+#Bound the places our bot can travel
+
+MIN_LATITUDE = os.environ.get('MIN_LATITUDE') or ''
+MAX_LATITUDE = os.environ.get('MAX_LATITUDE') or ''
+MIN_LONGITUDE = os.environ.get('MIN_LONGITUDE') or ''
+MAX_LONGITUDE = os.environ.get('MAX_LONGITUDE') or ''
+
+#API URL stuff
+
+DIRECTIONS_API_PREFIX = 'https://maps.googleapis.com/maps/api/directions/json?'
+STREETVIEW_API_PREFIX = 'https://maps.googleapis.com/maps/api/streetview?'
+
+#API Keys
+
+DIRECTIONS_API_KEY = os.environ.get('DIRECTIONS_API_KEY')
+STREETVIEW_API_KEY = os.environ.get('STREETVIEW_API_KEY')
