853075cfc1f3f1b9aef7a2bc87fa1e217f9e582a
Ryan Sommers <ryansommers@gmail.com>
2016-11-26 02:51:44 -0500

    Completed response retrieval and json parse

--- trip.py
+++ trip.py
@@ -1,8 +1,8 @@
-import map
+import urllib.request, json
 from config import DIRECTIONS_API_PREFIX, DIRECTIONS_API_KEY
 
-def getDirectionsResponse(trip, avoidHighways=True):
-	## takes Trip dict from builTrip, checks for waypoints, builds request and gets response
+def getDirectionsURL(trip, avoidHighways=True):
+	## takes Trip dict from buildTrip, checks for waypoints, builds request and gets response
 	
 	waypoints = []
 	
@@ -11,19 +11,19 @@ def getDirectionsResponse(trip, avoidHighways=True):
 			waypoints.append(value['latLong'])
 	
 	request = {
-		'prefix' : DIRECTIONS_API_PREFIX
-		'origin' : trip['origin']['latLong']
-		'destination' : trip['destination']['latLong']
-		'waypoints' : waypoints
+		'prefix' : DIRECTIONS_API_PREFIX,
+		'origin' : trip['origin']['latLong'],
+		'destination' : trip['destination']['latLong'],
+		'waypoints' : waypoints,
 		'key' : DIRECTIONS_API_KEY }
 
-	requestURL = '{prefix}origin={origin[0]},{origin[1]}&destination={destination[0]},{destination[1]}'
+	requestURL = '{prefix}origin={origin[0]},{origin[1]}&destination={destination[0]},{destination[1]}'.format(**request)
 		
 	if waypoints:
 		requestURL += '&waypoints='
 			
 		for waypoint in waypoints:
-			requestURL+= 'via={0[0]}%2c{0[1]}%7c'.format(waypoint)
+			requestURL+= 'via:{0[0]}%2c{0[1]}%7c'.format(waypoint)
 		
 	if avoidHighways:
 	
@@ -31,4 +31,21 @@ def getDirectionsResponse(trip, avoidHighways=True):
 		
 	requestURL += '&key={0}'.format(request['key'])
 	
-	pass
+	return requestURL
+	
+def getDirectionsResponse(requestURL):
+	## takes request URL from getDirectionsURL and retrieves response
+	
+	with urllib.request.urlopen(requestURL) as response:
+		directionsResponse = response.read().decode('utf8')
+		
+	serialResponse = json.loads(directionsResponse)
+	
+	return serialResponse
+
+def parseDirectionsResponse(response):
+	
+	if response['status'] != 'OK':
+		return None
+		
+	
