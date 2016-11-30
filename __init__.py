c896cb065700567c3ba7fad0e9ac45ab9b3de830
Ryan Sommers <ryansommers@gmail.com>
2016-11-30 13:12:45 -0500

    Added directions URL

--- __init__.py
+++ __init__.py
@@ -24,6 +24,9 @@ while True:
 	
 		replyTo = tweet.getPreviousID()
 		
+		mapURL = 'https://www.google.com/maps/dir/{0[0]},{0[1]}/{1[0]},{1[1]}/{2[0]},{2[1]}/am=t/data=4m4!4m3!2m1!1b1!3e0'.format(
+			newTrip.start.coord, step.start.coord, newTrip.end.coord)
+		
 		if previousInterval >= MINIMUM_TWEET_INTERVAL or int(step.duration['value']) >= MINIMUM_OVERRIDE_DURATION:
 			
 			previousInterval = int(step.duration['value'])
