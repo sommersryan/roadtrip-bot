03ce84e9ca981a7a2bff16c50b922dad355d5074
Ryan Sommers <ryansommers@gmail.com>
2016-12-03 01:13:56 -0500

    Fixed config vars

--- static.py
+++ static.py
@@ -1,10 +1,10 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
-from config import S3_BUCKET
+from config import AWS_S3_BUCKET
 import map
 
 storageConnection = S3Connection()
-datastore = storageConnection.get_bucket(S3_BUCKET)
+datastore = storageConnection.get_bucket(AWS_S3_BUCKET)
 
 def getLastLocation():
 	rawCoordinates = datastore.get_key('lastlocation').get_contents_as_string().decode('utf8')
