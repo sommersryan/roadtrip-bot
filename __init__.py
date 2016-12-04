890c715242c25ed27878404ce7a66c6eca235c0a
Ryan Sommers <ryansommers@gmail.com>
2016-12-04 11:53:45 -0500

    Added trip resume support to init

--- __init__.py
+++ __init__.py
@@ -4,45 +4,52 @@ from urllib.error import HTTPError
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-lastLoc = static.getLastLocation()
-
-departurePoint = map.Place(lastLoc[0],lastLoc[1])
-
+departurePoint = static.getLastLocation()
 logging.info("Retrieved departure point: {0}".format(departurePoint))
-	
-newPlan = map.Plan.toRandom(departurePoint)
 
-logging.info("Plan made: {0} to {1}".format(newPlan.origin.coord, newPlan.destination.coord))
-
-while True: 
-	
+if static.getIsTrip():
+	logging.info("In progress trip detected. Resuming.")
+	dest = static.getLastDestination()
+	newPlan = map.fromToCoords(departurePoint, dest) 
 	newTrip = trip.Trip.fromPlan(newPlan)
-	
-	if newTrip:
-		break
-	
-	else:
-		logging.info("Bad response. Making new plan from {0}".format(departurePoint.coord))			
-		newPlan = map.Plan.toRandom(departurePoint)
 
-logging.info("Response obtained. {0} steps.".format(len(newTrip.legs[0].steps)))
+else:
+	newPlan = map.Plan.toRandom(departurePoint)
 
-tripURL = 'https://www.google.com/maps/dir/{0[0]},{0[1]}/{1[0]},{1[1]}/am=t/data=4m4!4m3!2m1!1b1!3e0'.format(
+	logging.info("Plan made: {0} to {1}".format(newPlan.origin.coord, newPlan.destination.coord))
+
+	while True: 
+	
+		newTrip = trip.Trip.fromPlan(newPlan)
+	
+		if newTrip:
+			break
+	
+		else:
+			logging.info("Bad response. Making new plan from {0}".format(departurePoint.coord))			
+			newPlan = map.Plan.toRandom(departurePoint)
+
+	logging.info("Response obtained. {0} steps.".format(len(newTrip.legs[0].steps)))
+
+	tripURL = 'https://www.google.com/maps/dir/{0[0]},{0[1]}/{1[0]},{1[1]}/am=t/data=4m4!4m3!2m1!1b1!3e0'.format(
 		newTrip.start.coord, newTrip.end.coord)
 
-try:		
-	shortTripURL = tweet.urlShorten(tripURL)
+	try:		
+		shortTripURL = tweet.urlShorten(tripURL)
 
-except HTTPError:
-	shortTripURL = ''
+	except HTTPError:
+		shortTripURL = ''
 	
-preamble = 'New trip! {0} to {1}. {2}, {3} {4}'.format(newTrip.start.mediumDetail, 
-	newTrip.end.mediumDetail, newTrip.legs[0].distance['text'], newTrip.legs[0].duration['text'], shortTripURL)
+	preamble = 'New trip! {0} to {1}. {2}, {3} {4}'.format(newTrip.start.mediumDetail, 
+		newTrip.end.mediumDetail, newTrip.legs[0].distance['text'], newTrip.legs[0].duration['text'], shortTripURL)
 
-tweet.makeTweet(status=preamble, lat=newTrip.start.coord[0], long=newTrip.start.coord[1])
+	tweet.makeTweet(status=preamble, lat=newTrip.start.coord[0], long=newTrip.start.coord[1])
+	
+	static.saveIsTrip(True)
+	
+	time.sleep(60)
 
-time.sleep(60)
-
+static.saveLastDestination(newTrip.end)
 previousInterval = MINIMUM_TWEET_INTERVAL + 1
 
 for step in newTrip.legs[0].steps:
@@ -73,15 +80,20 @@ for step in newTrip.legs[0].steps:
 			image = view.getViewObject(step.start.coord)
 			tweet.makeTweetWithImage(stepTweet, image, replyTo, lat=step.start.coord[0], long=step.start.coord[1])
 			logging.info("Tweet posted. Sleeping for {0} seconds".format(step.duration['value']))
+			static.saveLocation(step.end)
 			time.sleep(int(step.duration['value']))
 			continue
 		
-		tweet.makeTweet(stepTweet,replyTo=replyTo, lat=step.start.coord[0], long=step.start.coord[1])
-		logging.info("Tweet posted. Sleeping for {0} seconds".format(step.duration['value']))
-		time.sleep(int(step.duration['value']))
-	
+		else:
+			tweet.makeTweet(stepTweet,replyTo=replyTo, lat=step.start.coord[0], long=step.start.coord[1])
+			logging.info("Tweet posted. Sleeping for {0} seconds".format(step.duration['value']))
+			static.saveLocation(step.end)
+			time.sleep(int(step.duration['value']))
+			continue
+			
 	previousInterval = int(step.duration['value'])
 	logging.info("Tweet not permitted for this step. Sleeping for {0}".format(step.duration['value']))
+	static.saveLocation(step.end)
 	time.sleep(int(step.duration['value']))
 
 time.sleep(60)
@@ -95,3 +107,5 @@ tweet.makeTweet(signOut,replyTo=replyTo, lat=newTrip.end.coord[0], long=newTrip.
 static.saveLocation(newTrip.end)
 
 logging.info("Departure point reset to {0}. Exiting.".format(departurePoint.coord))
+
+static.saveIsTrip(False)
