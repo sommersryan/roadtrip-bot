de17f9eb747adf644da28ffe4107ce4cd8f3e828
Ryan Sommers <ryansommers@gmail.com>
2016-12-14 21:12:08 -0500

    Added setting of SinceID

--- collect.py
+++ collect.py
@@ -6,5 +6,10 @@ if suggestions:
 
 	for suggestion in suggestions:
 	
-		static.saveSuggestion(suggestion)		
+		static.saveSuggestion(suggestion)
 
+IDs = [str(item[1].id) for item in suggestions]
+
+IDs.sort()
+
+static.setSinceID(IDs[-1])
